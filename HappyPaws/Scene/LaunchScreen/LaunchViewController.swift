d20aadcb4215d2bb95cb6b3d801ad0fe4c5b50a3
nino <nino.dimitriadi.1@btu.edu.ge>
2025-01-13 13:33:28 +0400

    Add launchScreen

--- HappyPaws/Scene/LaunchScreen/LaunchViewController.swift
+++ HappyPaws/Scene/LaunchScreen/LaunchViewController.swift
@@ -6,24 +6,103 @@
 //
 
 import UIKit
+import SwiftUI
 
 class LaunchViewController: UIViewController {
 
+    private lazy var logoImageView: UIImageView = {
+        let imageView = createImageView(imageName: "HappyPowsLogo", contentMode: .scaleAspectFit)
+        if let logoImage = UIImage(named: "HappyPowsLogo") {
+            let coloredImage = logoImage.withRenderingMode(.alwaysTemplate)
+            imageView.image = coloredImage
+            imageView.tintColor = .orange
+        }
+        return imageView
+    }()
+
+    private lazy var gifImageView: UIImageView = {
+        return createImageView(imageName: nil, contentMode: .scaleAspectFit)
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+        configureLayout()
+        loadGIF()
+        startTransitionToMainApp()
+    }
 
-        // Do any additional setup after loading the view.
+    private func setupUI() {
+        setupImageViews()
+        view.backgroundColor = .white
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupImageViews() {
+        view.addSubview(logoImageView)
+        view.addSubview(gifImageView)
     }
-    */
 
+    private func createImageView(imageName: String?, contentMode: UIView.ContentMode) -> UIImageView {
+        let imageView = UIImageView()
+        imageView.contentMode = contentMode
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        if let imageName = imageName {
+            imageView.image = UIImage(named: imageName)
+        }
+        
+        return imageView
+    }
+
+    private func configureLayout() {
+        NSLayoutConstraint.activate([
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 350),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+
+        NSLayoutConstraint.activate([
+            gifImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            gifImageView.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: -100),
+            gifImageView.widthAnchor.constraint(equalToConstant: 300),
+            gifImageView.heightAnchor.constraint(equalToConstant: 300)
+        ])
+    }
+
+    private func loadGIF() {
+        if let walkGif = UIImage.gifImageWithName("playing") {
+            gifImageView.image = walkGif
+        }
+    }
+
+    private func startTransitionToMainApp() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.transitionToMainApp()
+        }
+    }
+
+    private func transitionToMainApp() {
+        let mainViewController = LogInViewController()
+        mainViewController.modalTransitionStyle = .crossDissolve
+        mainViewController.modalPresentationStyle = .fullScreen
+        present(mainViewController, animated: true, completion: nil)
+    }
+
+}
+
+@available(iOS 16.0, *)
+struct LaunchViewController_Preview: PreviewProvider {
+    static var previews: some View {
+        LaunchViewControllerRepresentable()
+    }
+}
+
+@available(iOS 16.0, *)
+struct LaunchViewControllerRepresentable: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> LaunchViewController {
+        return LaunchViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: LaunchViewController, context: Context) {}
 }
